1dcf1e447556019ecd9e4ce7b740dab2ebe82a90
Enrique Menjivar <mt16007@ues.edu.sv>
2019-01-28 23:41:25 -0600

    Connection to the database

---
+++ connection.php
@@ -0,0 +1,23 @@
+<?php 
+
+$link ='mysql:host=localhost; dbname=php_colors';
+$user = 'root';
+$pass = '';
+
+try {
+	$connection = new PDO($link, $user, $pass);
+
+	echo "Connected";
+
+	/*foreach ($connection->query('SELECT * FROM COLORS') as $row) {
+		print_r($row);
+	}*/
+
+} catch (PDOException $e) {
+	print "Error!: " . $e->getMessage() . "<br>";
+	die();
+} finally{
+	$connection = null;
+}
+
+?>
